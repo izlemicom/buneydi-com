c66ce7b748cb41fbe15473a2b651d9684817127d
izlemicom <izlemi.com@gmail.com>
2021-11-20 13:31:45 +0300

    ...

--- img/index.js
+++ img/index.js
@@ -4,13 +4,16 @@ var cors = require("cors");
 var app = express();
 
 app.use(cors());
-
+var count = 0;
 app.get("/:image", (req, res) => {
   var image = "../api/public/images" + `/${req.params.image}`;
 
   try {
     if (fs.existsSync(image)) {
       const stream = fs.createReadStream(image);
+      count++;
+      console.log(count);
+      console.log(req.params.image);
       stream.pipe(res);
     } else {
       console.error(`${req.params.image} bulunamadi.`);
